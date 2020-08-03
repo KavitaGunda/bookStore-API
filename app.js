10069bac610b88447b6f4c4e10a8049f97aa4e29
Kavita Gunda <gundakavita@gmail.com>
2020-08-04 00:11:18 +0530

    Implements - main app.js api server logic and .env file which stores ACCESS-TOKEN and REFRESH-TOKEN

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config();
+require('./database/database');
 
 var app = express();
 
@@ -13,22 +10,24 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Book store API Routes
+app.use(['/api/login', '/api/auth'], require('./routes/index'));
+app.use(['/register', '/api/createUser'], require('./routes/register'));
+app.use('/api/profile', require('./routes/profile'));
+app.use('/api/user', require('./routes/users'));
+app.use('/api/books', require('./routes/books'));
+app.use('/', (req, res) => res.send('The Book Store API Server Running.'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
